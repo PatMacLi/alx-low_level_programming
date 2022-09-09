795d718eb66eca3c2ecd771113498a51b84662f5
patrick Kiarie <pkiarie2011@gmail.com>
2022-09-09 16:45:55 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -20,10 +20,9 @@ int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return (0);
+	return (0);
 
 }
-
 
