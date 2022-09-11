042e395342441db7adf152ba1cb6415bac1d09cc
patrick Kiarie <pkiarie2011@gmail.com>
2022-09-11 13:37:54 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 
 
 /**
-* Main - entry point
+*main - entry point
 * Description- using the main function
-
+*
 * this program prints "Programming is like building a multilingual puzzle
-
+*
 * Return: Always return 0 (sources)
-
+*
 */
 
 int main(void)
