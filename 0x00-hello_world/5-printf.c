043fae973f29a988cf7656f60f0c14798332d1f9
patrick Kiarie <pkiarie2011@gmail.com>
2022-09-11 13:46:49 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,17 +3,16 @@
 
 
 /**
-
-* main - print the string in the put function
-
+*main - print the string in the put function
 *
-
+*
+*
 * Description: using the main function
-
+*
 * this program prints "Programming is like building a multilingual puzzle
-
+*
 * Return: 0
-
+*
 */
 
 int main(void)
